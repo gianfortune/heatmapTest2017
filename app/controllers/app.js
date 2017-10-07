90bf045fd149615ff577a93d7ac2ed48d0841014
Marisa Gianfortune <mng35@cornell.edu>
2017-10-07 12:06:31 -0400

    Initial set-up.  Project now has an angular controller as well as angular-material setup.

---
+++ app/controllers/app.js
@@ -0,0 +1,7 @@
+// Define the `heatmapApp` module
+var heatmapApp = angular.module('heatmapApp', ['ngMaterial'])
+
+// Define the `PhoneListController` controller on the `heatmapApp` module
+heatmapApp.controller('heatmapController', function heatmapController () {
+  this.test = 'hi'
+})
